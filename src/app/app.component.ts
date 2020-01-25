f7767429efa0f559c54ee28cd20d2e233bc6f999
Chuck Waltz <chuckwaltz@gmail.com>
2020-01-25 10:00:22 -0500

    Updated glitch logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { NgwWowService } from 'ngx-wow';
+import { NgwWowService } from "ngx-wow";
 
-import 'rxjs/add/observable/interval';
+import "rxjs/add/observable/interval";
+import { I18nSelectPipe } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-
   private sub;
 
   headerBG: any;
@@ -18,87 +18,60 @@ export class AppComponent {
 
   widthThreshold = 1200;
 
-  headerBGs = ["url('assets/header-background-chuck-cropped.jpg')", "url('assets/header-background-chuck-cropped-glitch1.jpg')", "url('assets/header-background-chuck-cropped-glitch2.jpg')", "url('assets/header-background-chuck-cropped-glitch3.jpg')", "url('assets/header-background-chuck-cropped-glitch4.jpg')", "url('assets/header-background-chuck-cropped-glitch5.jpg')", "url('assets/header-background-chuck-cropped-glitch6.jpg')"];
+  headerBGDefault = "url('assets/header-background-chuck-cropped.jpg')";
+  headerBGs = [
+    "url('assets/header-background-chuck-cropped-glitch1.jpg')",
+    "url('assets/header-background-chuck-cropped-glitch2.jpg')",
+    "url('assets/header-background-chuck-cropped-glitch3.jpg')",
+    "url('assets/header-background-chuck-cropped-glitch4.jpg')",
+    "url('assets/header-background-chuck-cropped-glitch5.jpg')",
+    "url('assets/header-background-chuck-cropped-glitch6.jpg')"
+  ];
 
-  constructor(private wowService: NgwWowService){
+  constructor(private wowService: NgwWowService) {}
 
-    window.addEventListener('resize', this.goMobile);
-
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     this.wowService.init();
 
-    this.headerBG = document.getElementById('header-background');
-    this.headerBGswap = document.getElementById('header-background-swap');
-    this.headerBG.style.backgroundImage = this.headerBGs[0];
-
-    this.goMobile();
-
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      console.log("Mobile!");
-    }
+    this.headerBG = document.getElementById("header-background");
+    this.headerBGswap = document.getElementById("header-background-swap");
+    this.headerBG.style.backgroundImage = this.headerBGDefault;
 
     setTimeout(() => {
-    
-    this.glitchInit();
-  
-    }, 5000);
-    
-    
+      this.glitch();
+    }, 8500);
   }
 
-  scrollTo(elem){
+  scrollTo(elem) {
     document.querySelector(elem).scrollIntoView({
-      block: 'start',
-      behavior: 'smooth'
-    })
+      block: "start",
+      behavior: "smooth"
+    });
   }
 
-  glitchInit(){
-    setInterval(() => {
-      this.glitch()
-    }, 5000); //Interval for BG swap
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  randomNumber(){
-    return (Math.random() * 220) + 100;
+  async glitch() {
+    this.headerBGswap.style.backgroundImage = this.headerBGs[
+      Math.floor(Math.random() * this.headerBGs.length)
+    ];
+
+    await this.sleep(500);
+
+    this.headerBGswap.style.opacity = "1";
+    this.headerBG.style.opacity = "0";
+
+    setTimeout(async () => {
+      this.headerBGswap.style.opacity = "0";
+      this.headerBG.style.opacity = "1";
+      await this.sleep(this.randomNumber(250, 5000));
+      this.glitch();
+    }, this.randomNumber(100, 800));
   }
 
-  goMobile(){ //Fixes for various mobile issues.
-    console.log(window.innerWidth);
-    if(window.innerHeight > window.innerWidth){ //Check for Portrait orientation
-      document.getElementById('header-content').style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
-      document.getElementById('header-content').style.maxWidth = 'unset';
-      document.getElementById('header-content').style.paddingBottom = '8vw';
-      document.getElementById('header-background').style.backgroundPositionX = 'left';
-      document.getElementById('header-background-swap').style.backgroundPositionX = 'left';
-    }else{
-      document.getElementById('header-content').style.backgroundColor = 'unset';
-      document.getElementById('header-background').style.backgroundPositionX = 'right';
-      document.getElementById('header-background-swap').style.backgroundPositionX = 'right';
-
-      if( /Android|iPhone|iPod|BlackBerry|IEMobile/i.test(navigator.userAgent) ) {
-        console.log("Mobile!");
-        document.getElementById('header-content').style.maxWidth = '70vw';
-        document.getElementById('header-content').style.paddingBottom = '4vw';
-      }
-    }
+  randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
   }
-
-  glitch(){
-    //this.headerBG.style.backgroundImage = this.headerBGs[Math.floor(Math.random() * this.headerBGs.length)];
-    this.headerBGswap.style.backgroundImage = this.headerBGs[Math.floor(Math.random() * this.headerBGs.length)];
-
-    setTimeout(() => {
-      //this.headerBG.style.backgroundImage = this.headerBGs[0];
-      this.headerBG.style.opacity = '0';
-      this.headerBGswap.style.opacity = '1';
-      setTimeout(() => {
-        this.headerBGswap.style.opacity = '0';
-        this.headerBG.style.opacity = '1';
-      }, this.randomNumber())
-    }, 500);
-  }
-
 }
